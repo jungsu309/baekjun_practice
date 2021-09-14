010f1442f623aa006d5f788166b432d973eda17d
한정수 <jungsu_@naver.com>
2021-09-14 22:53:32 +0900

    Update 210911_14장정렬-23-국영수.py

--- 2021_codingtest/정렬/210911_14장정렬-23-국영수.py
+++ 2021_codingtest/정렬/210911_14장정렬-23-국영수.py
@@ -2,6 +2,8 @@
 
 #이름, 국어, 영어, 수학 순으로 입력받음
 #국어 감소, 영어 증가, 수학 감소 순으로 정렬 후 이름만 출력
+import sys
+input = sys.stdin.readline
 
 N = int(input())
 array=[]
@@ -11,7 +13,7 @@ for n in range(N):
 #print(array)
 
 #구글에 검색해봄.
-array.sort(key = lambda x: (-x[1], x[2], -x[3]))
+array.sort(key = lambda x: (-x[1], x[2], -x[3], x[0]))
 #print(array)
 
 for i in range(N):
